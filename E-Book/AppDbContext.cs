8847dd15a9bd1e04ce26db6fabd90b7cd77d3b02
Abdelwafi-Borae <156581667+Abdelwafi-Borae@users.noreply.github.com>
2024-09-23 18:37:50 +0400

    Add project files.

---
+++ E-Book/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Book_DataAccess.Data
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext( DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+       
+    }
+}
